ad060291467c3416b2620594434c0fcd2dc88af1
chrisgitgot <chrishenagerdesign@gmail.com>
2026-08-26 15:14:16 -0700

    restyle navigation text

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import '../styles/Navigation.css'
 function Navigation() {
     return (
         <div className="navigation">
-            <h1>-a game studio in Portland, Oregon-</h1>
+            <h1>a game studio in Portland, Oregon</h1>
         </div>
     )
 }
